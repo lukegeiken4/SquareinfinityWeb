60541cd84f2203f870ac95b93f8a3be71aa64402
Luke Geiken <lukedrum4@gmail.com>
2014-05-27 12:20:06 -0600

    Initial commit for original project

---
+++ games/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Square Infinity Games</title>
+	<link rel="shortcut icon" href="img/favicon.gif" />
+	<link href="css/sIGamesStylesheet.css" rel="stylesheet" type="text/css"/>
+	<link href="../css/topNavbarStyle.css" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'>
+	<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<meta name = "description" content="As an Independent game creator, the Square Infinity games team is looking to push the limits of storytelling and gameplay from where the best games are created, our parent's basement."/>
+	<meta property="og:image" content="http://www.squareinfinity.com/gmeier/images/stretchLogo3.png"/>
+	<meta property="og:site_name" content="Square Infinity Games"/>
+	<meta property="og:title" content="Square Infinity Games"/>
+	<meta property="og:url" content = "http://www.squareinfinity.com/"/>
+</head>
+
+<body >
+	<div class="top-navbar">
+		<a  href= "../tech/index.php"> <div class="topnavbutton">Square Infinity Tech</div></a>
+		<a href="http://blog.squareinfinity.com"><div class="topnavbutton">Square Infinity Blog</div></a>
+	</div>
+	
+	<div class="header">headerImageHere</div>
+	
+	<div class="navbar">
+		<div class="navbutton"></div>
+	</div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<title>Square Infinity</title>
+	<link rel="shortcut icon" href="images/favicon.gif" />
+	<link href="squareInfinityStylesheet.css" rel="stylesheet" type="text/css"/>
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'>
+	<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<meta name = "description" content="Square Infinity combines professional web and mobile application contracting with the quirky goodness of Indie game development. Come join us on our journey!"/>
+	<meta property="og:image" content="http://www.squareinfinity.com/gmeier/images/stretchLogo3.png"/>
+	<meta property="og:site_name" content="Square Infinity"/>
+	<meta property="og:title" content="Square Infinity"/>
+	<meta property="og:url" content = "http://www.squareinfinity.com/"/>
+</head>
+
+<body>
+	<div class="main">
+		<div class="gameBackground"></div>
+		<div class="techBackground"></div>
+		<div class="centerImage"><img src="images/SIsplitLogo.png" style="box-shadow:none;"/></div>
+		<a href="games/index.php"><div class="button" id="game">Square Infinity Games</div></a>
+		<a href="tech/index.php"><div class="button" id="tech">Square Infinity Technology</div></a>
+	</div>
+</body>
+
+</html>
