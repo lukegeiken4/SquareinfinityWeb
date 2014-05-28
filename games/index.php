98103f4b241746ade2cb72c1059d410e21425f4c
Luke Geiken <lukedrum4@gmail.com>
2014-05-28 13:50:00 -0600

    Started inital build of SI games website

--- games/index.php
+++ games/index.php
@@ -6,7 +6,7 @@
 	<title>Square Infinity Games</title>
 	<link rel="shortcut icon" href="img/favicon.gif" />
 	<link href="css/sIGamesStylesheet.css" rel="stylesheet" type="text/css"/>
-	<link href="../css/topNavbarStyle.css" rel="stylesheet" type="text/css">
+	<!--<link href="../css/topNavbarStyle.css" rel="stylesheet" type="text/css">-->
 	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'>
 	<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<meta name = "description" content="As an Independent game creator, the Square Infinity games team is looking to push the limits of storytelling and gameplay from where the best games are created, our parent's basement."/>
@@ -17,16 +17,25 @@
 </head>
 
 <body >
-	<div class="top-navbar">
-		<a  href= "../tech/index.php"> <div class="topnavbutton">Square Infinity Tech</div></a>
-		<a href="http://blog.squareinfinity.com"><div class="topnavbutton">Square Infinity Blog</div></a>
+	<div style="position: absolute; width: 100%; height:20%; z-index:1;">
+		<img src="img/whiteSILOGOICON.png" style="margin-top:2%; display: block; width: 10%; height:60%; z-index:1; margin-left: auto; margin-right: auto;" />
+		<a class="nav-headers" id="home-head" href= "../tech/index.php"><div style="margin-top: 6%;">HOME</div></a>
+		<a class="nav-headers" id="games-head" href= "../tech/index.php"><div style="margin-top: 6%;">GAMES</div></a>
+		<a class="nav-headers" id="team-head" href= "../tech/index.php"><div style="margin-top: 6%;">TEAM</div></a>
+		<a class="nav-headers" id="contact-head" href= "../tech/index.php"><div style="margin-top: 6%;">CONTACT</div></a>
 	</div>
-	
+	<div class="top-navbar" style="position: absolute; z-index:2;">
+		<div class="top-navbar-bdiv" style="margin-right:5%; border-right:1px solid white;";>
+			<a class="other-sites" href= "../tech/index.php"> <div class="topnavbutton">Square Infinity Tech</div></a>
+		</div>
+		<div class="top-navbar-bdiv">
+			<a class="other-sites" href="http://blog.squareinfinity.com"><div class="topnavbutton">Square Infinity Blog</div></a>
+		</div>
+	</div>
+	<div class="main-navbar" style="position: absolute; z-index:-2;">
+		<img src="img/gamebanner.png" style="width:100%; height:230%;" />
+	</div>
 	<div class="header">headerImageHere</div>
-	
-	<div class="navbar">
-		<div class="navbutton"></div>
-	</div>
 </body>
 
 </html>
