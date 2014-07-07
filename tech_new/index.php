8683817e915c061598ff77fdf19793e9d8293717
Ivan Mattie <ivan.mattie@squareinfinity.com>
2014-07-07 15:05:23 -0600

    Added New Tech Site
    Added new tech site and basic information for that site, including a stylesheet

---
+++ tech_new/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Square Infinity Technologies</title>
+		<link rel="shortcut icon" href="img/favicon.gif" />
+		<link href="css/sIGamesStylesheet.css" rel="stylesheet" type="text/css"/>
+		<!--<link href="../css/topNavbarStyle.css" rel="stylesheet" type="text/css">-->
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'>
+		<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+		<meta name = "description" content="As an Independent game creator, the Square Infinity games team is looking to push the limits of storytelling and gameplay from where the best games are created, our parent's basement."/>
+		<meta property="og:image" content="http://www.squareinfinity.com/gmeier/images/stretchLogo3.png"/>
+		<meta property="og:site_name" content="Square Infinity Games"/>
+		<meta property="og:title" content="Square Infinity Games"/>
+		<meta property="og:url" content = "http://www.squareinfinity.com/"/>
+		
+		<link href="style.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div class="top-bar shadow">
+			
+		</div>
+		
+		<div class="center-image-container shadow">
+			<img class="center-image" src="http://squareinfinity.com/images/stretchLogo3.png" />
+		</div>
+	</body>
+</html>
