abb6879b09b904c5db03eaa36f7e69924d840c8d
Ivan Mattie <ivan.mattie@squareinfinity.com>
2014-07-07 21:12:41 -0600

    Added More Styling & Mobile Support
    Design now scales reactively to mobile, and I added a whole bunch of new links and button styles.

--- tech_new/index.php
+++ tech_new/index.php
@@ -3,16 +3,19 @@
 <html>
 	<head>
 		<title>Square Infinity Technologies</title>
+		
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+		
+		<!--
 		<link rel="shortcut icon" href="img/favicon.gif" />
 		<link href="css/sIGamesStylesheet.css" rel="stylesheet" type="text/css"/>
-		<!--<link href="../css/topNavbarStyle.css" rel="stylesheet" type="text/css">-->
 		<link href='http://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'>
 		<script  src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 		<meta name = "description" content="As an Independent game creator, the Square Infinity games team is looking to push the limits of storytelling and gameplay from where the best games are created, our parent's basement."/>
 		<meta property="og:image" content="http://www.squareinfinity.com/gmeier/images/stretchLogo3.png"/>
-		<meta property="og:site_name" content="Square Infinity Games"/>
+		<meta property="og:site_name" content="Square Infinity Technologies"/>
 		<meta property="og:title" content="Square Infinity Games"/>
-		<meta property="og:url" content = "http://www.squareinfinity.com/"/>
+		<meta property="og:url" content = "http://www.squareinfinity.com/"/> <!-- change this before release -->
 		
 		<link href="style.css" rel="stylesheet" type="text/css" />
 	</head>
@@ -20,9 +23,24 @@
 		<div class="top-bar shadow">
 			
 		</div>
-		
+		<div class="mobile-div">
+			<button class="top-nav-button nav-button-start-left">PORTFOLIO</button>
+			<button class="top-nav-button">CONTACT</button>
+		</div>
 		<div class="center-image-container shadow">
 			<img class="center-image" src="http://squareinfinity.com/images/stretchLogo3.png" />
+		</div>		
+		
+		<div class="floating-sidebar top-division">
+			<h1 class="mobile-center">Navigation</h1>
+			<button class="sidebar-link">Web Design</a>
+			<button class="sidebar-link">App Design</a>
 		</div>
+		
+		<div class="main-content top-division">
+			This is the main content
+		</div>
+		
+		
 	</body>
 </html>
