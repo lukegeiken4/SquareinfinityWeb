859ee82fb0522e39b5d3d7723b41b8c6bac0f364
Ziggy222 <andreweagle@Andrew-Eagles-MacBook-Pro.local>
2014-07-09 09:18:48 -0600

    Minor changes and content addition to tech website

--- tech_new/index.php
+++ tech_new/index.php
@@ -1,8 +1,51 @@
-<!DOCTYPE html>
-
 <html>
-	<head>
-		<title>Square Infinity Technologies</title>
+<?php
+
+$return = $_GET['page'];
+
+if($return == "contact"){
+	getHead("Contact Square Infinity");
+	echo "<body>";
+	getTopBar();
+	getMainContent("Feel free to contact us. Find emails below or connect over social media. (this is where social media links go)<br>
+		       For contracts and quotes:  contracts@squareinfinity.com<br>
+		       For general questions and information: tech.contact@squareinfinity.com");
+	echo "</body>";
+} else if ($return  == "portfolio"){
+	getHead("Square Infinity Projects");
+	echo "<body>";
+	getTopBar();
+	getSideBar("portfolio");
+	getMainContent("Web Projects:<br>
+		       Project 1 Name for example purposes.<br>
+		       Project blurb... Here we will describe the project if it needs any sort of description.<br>
+		       Link to the website from the project.<br>
+		       Somewhere off to one side will be a picture I am sure of the website.<br>
+		       
+		       Project 2 Name and stuff.<br>
+		       
+		       Then a ways down, Software Projects and the same format but for our apps and such.");
+	echo "</body>";
+} else{
+	getHead("Square Infinity Technologies");
+	echo "<body>";
+	getTopBar();
+	getSideBar("main");
+	getMainContent(getFrontPageContent());
+	getProjectSpecificsContent();
+	echo "</body>";
+}
+
+function getHead($titleName){
+	?>
+		<head>
+		<title><?php echo $titleName; ?></title>
+		
+		<script>
+			function href(location){
+				document.location.href = location;
+			}
+		</script>
 		
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		
@@ -18,29 +61,88 @@
 		<meta property="og:url" content = "http://www.squareinfinity.com/"/> <!-- change this before release -->
 		
 		<link href="style.css" rel="stylesheet" type="text/css" />
-	</head>
-	<body>
-		<div class="top-bar shadow">
+		</head>
+	<?php
+}
+
+function getTopBar(){
+	?>
+	<div class="top-bar shadow">
 			
-		</div>
-		<div class="mobile-div">
-			<button class="top-nav-button nav-button-start-left">PORTFOLIO</button>
-			<button class="top-nav-button">CONTACT</button>
-		</div>
-		<div class="center-image-container shadow">
-			<img class="center-image" src="http://squareinfinity.com/images/stretchLogo3.png" />
-		</div>		
-		
-		<div class="floating-sidebar top-division">
-			<h1 class="mobile-center">Navigation</h1>
-			<button class="sidebar-link">Web Design</a>
-			<button class="sidebar-link">App Design</a>
-		</div>
-		
-		<div class="main-content top-division">
-			This is the main content
-		</div>
-		
-		
-	</body>
+	</div>
+	<div class="mobile-div">
+		<button class="top-nav-button nav-button-start-left" onclick="href('http://dev.squareinfinity.com/tech/?page=portfolio')">PORTFOLIO</button>
+		<button class="top-nav-button" onclick="href('http://dev.squareinfinity.com/tech/?page=contact')">CONTACT</button>
+	</div>
+	<div class="center-image-container shadow">
+		<a href="http://dev.squareinfinity.com/tech">
+		<img class="center-image" src="http://squareinfinity.com/images/stretchLogo3.png" />
+		</a>
+	</div>
+	<?php
+}
+
+function getMainContent($someData){
+?>
+<div class="main-content top-division">
+		<?php echo $someData; ?>
+	</div>
+<?php
+}
+
+function getFrontPageContent(){
+	return "At Square Infinity Technologies, we provide a wide array of software solutions customized to meet your needs.
+	With our small team and experience in the cutting edge of software design, Square Infinity Technologies offers fast turnaround times
+	while delivering a product that includes a sleek user experience. For each project we take an individual look at which elements
+	provide the greatest value to the specific project. Square Infinity Technologies is open to working on software projects of any
+	scope, style, or genre. A few of our specialties are listed below in more detail.";
+}
+
+function getProjectSpecificsContent(){
+?>
+<div class="main-content">
+	<h1 class="main-content-header" id="web-design">Web Design</h1>
+	This is where we will talk about all of the web desing things we do! Like PHP! And HTML! And JavaScript! Who knew that we were so
+	talented! Anyway, we could even talk about some basic price points if we wanted to here... But I don't really know if that is necessary.
+	<h1 class="main-content-header" id="software-design">Software Design</h1>
+	This is where I will talk about all of the software things that we do! Like android, iOS, 
+</div>
+<?php
+}
+
+function getSideBar($content){
+?>
+<div class = "floating-sidebar top-division">
+	<?php
+		if($content == "main"){
+			?>
+				<h1 class="mobile-center">Navigation</h1>
+				<button class="sidebar-link" onclick="href('http://dev.squareinfinity.com/tech/#web-design')">Web Design</button>
+				<button class="sidebar-link">App Design</button>
+			<?php
+		}else if($content == "portfolio"){
+			?>
+				<h1 class ="mobile-center">Navigation</h1>
+				<button class="sidebar-link">Project 1</button>
+				<button class="sidebar-link">Project 2</button>
+			<?php
+		}
+	?>
+</div>
+<?php
+}
+
+function getFrontPageSideBarContent(){
+	return '
+	<h1 class="mobile-center">Navigation</h1>
+	<button class="sidebar-link">Web Design</button>
+	<button class="sidebar-link">App Design</button>';
+}
+function getPortfolioSideBarContent(){
+	return '
+	<h1 class ="mobile-center">Navigation</h1>
+	<button class="sidebar-link">Project 1</button>
+	<button class="sidebar-link">Project 2</button>';
+}
+?>
 </html>
