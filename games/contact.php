6e5883f295e37c67a93533ec8c4e41a3dc197a66
Luke Geiken <lukedrum4@gmail.com>
2014-07-10 15:08:28 -0600

    Added css to the contact page

--- games/contact.php
+++ games/contact.php
@@ -7,26 +7,26 @@
 <body >
 	<?php getMainBar('Mail'); ?>
 	<div style="position: absolute; overflow: auto; width:100%; height:100%; top:0; z-index: 0;">
-		<div style="width:100%; height:100%; background: #FFB84E; position: relative;">
-			<div class="socialMedia" style="height:100%; width:50%;left:0px;position:absolute">
-				<div style="width:50%;left:0px;margin-left:20%; margin-top: 30%;">
+		<div style="width:100%; height:100%; position: relative;">
+			<div class="socialMedia" style="height:100%; width:50%;left:0px;position:absolute;">
+				<div style="width:50%;left:0px;margin-left:20%; margin-top: 30%; background: black; padding: 3%; padding-right: 5%; border: 5px solid white;">
 					<?php
 					// display form if user has not clicked submit
 					if (!isset($_POST["submit"])){
 					?>			
 						<form name="contactform" method="post" action="" autocomplete="off" onsubmit="return validateForm()">
 							<div style="width:50%; position: relative;  margin-top: 10%;">
-								<h2 style="position: absolute; font-family: Copperplate;" >Name: </h2>
+								<h2 style="position: absolute; font-family: Copperplate; color: white;" >Name: </h2>
 								<input style="position: absolute; margin-left:80%; margin-top:15%; width: 100%;" type="text" name="name" required/><br>
 							</div>
 							
 							<div style="width:50%; position: relative; margin-top: 10%;">
-								<h2 style="position: absolute; font-family: Copperplate;">Email: </h2>
+								<h2 style="position: absolute; font-family: Copperplate;  color: white;">Email: </h2>
 								<input style="position: absolute; margin-left:80%;  margin-top:15%; width: 100%;" type="text" name="from" required><br>
 							</div>
 							
 							<div style="width:50%; position: relative; margin-top: 10%;">
-							  <h2 style="position: absolute; font-family: Copperplate;">Message: </h2>
+							  <h2 style="position: absolute; font-family: Copperplate;  color: white;">Message: </h2>
 							  <textarea style="margin-top:35%;" rows="10" cols="50" name="message" style="resize:vertical" ></textarea><br>
 							</div>
 							
@@ -54,9 +54,14 @@
 				</div>
 			</div>
 			<div class="socialMedia" style="height:100%; width:50%;right:0px; position:absolute">
-				<a href="https://www.facebook.com/squareInfinityTech" target="_blank"><div class="social" style="background-image: url(img/46-facebook.svg); top:20%; left:40%;"></div></a>
-				<a href="http://www.twitter.com/squareInfinity" target="_blank"><div class="social" style="background-image: url(img/45-twitter.svg); top:55%; left:10%;"></div></a>
-				<a href="https://plus.google.com/103430757959795440005" target="_blank"><div class="social" style="background-image: url(img/80-google-plus.svg); top:55%; left:70%;"></div></a>
+				<div style="margin-top:20%; width:80%; height:70%; background: white; border: 5px solid black; margin-left:auto; margin-right:auto;">
+					<a href="https://www.facebook.com/squareInfinityTech" target="_blank"><div class="social" style="background-image: url(img/46-facebook.svg); top:20%; left:35%;"></div></a>
+					<a href="http://www.twitter.com/squareInfinity" target="_blank"><div class="social" style="background-image: url(img/45-twitter.svg); top:55%; left:10%;"></div></a>
+					<a href="https://plus.google.com/103430757959795440005" target="_blank"><div class="social" style="background-image: url(img/80-google-plus.svg); top:55%; left:60%;"></div></a>
+					<div style="position: relative; margin-left: 30%; margin-top:50%;">
+						<h2 style="font-family: Copperplate; font-size: 40px;">Follow Us!</h2>
+					</div>
+				</div>
 			</div>
 		</div>	
 	</div>
