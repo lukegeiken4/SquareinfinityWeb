7fefaff91ff47a8c6897e6479f3cc22b70221e92
ghmeier <meier.garret@gmail.com>
2014-07-16 11:04:00 -0600

    adding pretty banners

--- games/games.php
+++ games/games.php
@@ -25,12 +25,12 @@
 				<div class="button TravelQuestDesc " id="three"><i class="fa fa-paw fa-2x fa-fw" style=""></i></div>
 			</div>
 			<div class="gamesQuick" id="TravelQuestTitle">
-				<strong>Release Date:</strong> A Wintry Eve 2014
+				<strong>Release Date:</strong> A Wintry Eve 2014</vr>
 				<strong>Platforms:</strong> Android, iOS
 			</div>
 			<div class="contents">
 				<div class="gamesInfo TravelQuestDesc" id="one">
-					<div style="width:400px;">
+					<div style="width:25%;">
 					<p>
 						&nbsp;&nbsp;&nbsp;&nbsp;Fill up on gas and find your sunglasses because you don't have to 
 						be near Chicago to get the full road trip experience with our latest project. With TravelQuest,
@@ -51,22 +51,58 @@
 						&nbsp;&nbsp;&nbsp;&nbsp;Take your mobile gaming to the road with TravelQuest, where the game world
 						is the one we live in.
 					</p>
-					</div>
-					
+					</div>					
 				</div>
 				<div class="gamesInfo TravelQuestDesc" id="two">
-					<div style="width:400px">
+				<h1 style="text-align:center;margin-top:-50px;">Quest Types</h1>
+					<div style="position:absolute;display:inline-block;width:25%">
+					
 					<p>
-						&nbsp;&nbsp;&nbsp;&nbsp;Oooh more info.
+						<strong>Battle Tower -</strong> You will venture forth to the tallest buildings and battle your way through - floor by floor - to reach the top! Many enemies stand in the way. . . See if you can survive the gauntlet.
+					</p>
+					<p>
+						<strong>Caravan -</strong> By combining forces with valiant friends you can defeat far greater enemies than you could alone. Use your caravan of companions to slay monstrous beasts.
+					</p>
+					<p>
+						<strong>Champion -</strong> You will receive a legendary weapon. . . and use it to beat those nasty beasties! With monsters on all sides, the people need a champion. Do you have what it takes?
+					</p>
+					<p>
+						<strong>Commission -</strong> Your expertise at crafting has prompted someone to commission a piece. Get cracking! Oh, and don't forget to deliver the finished product!
+					</p>
+					<p>
+						<strong>Craft -</strong> You shall craft a thing and then be rewarded.
+					</p>
+					<p>
+						<strong>Hunt -</strong> You must travel far and wide to a new location to find your prey. Lucky for you, you know where they are! Go hunt those monsters.
+					</p>
+					</div>
+					<div style="position:absolute;display:inline-block;width:25%;left:27%">
+					<p>
+						<strong>Journey -</strong> When your heart is filled with the desire to get away - it is time for a journey. So go, you will see may things and you will face great danger but you will be the better for it.
+					</p>
+					<p>
+						<strong>Slay -</strong> The monsters near you grow fearless and abundant. It's up to you to take them down a notch! Slay enough and claim your reward!
+					</p>
+					<p>
+						<strong>Squirey -</strong> Battle makes the heart grow fonder. Fight alongside specific friends in order to strengthen your friendship, and also to get loot and glory and such.
+					</p>
+					<p>
+						<strong>Track -</strong> Your enemies have evaded and escaped you. . . but they've left a trail. Might as well give chase and follow them to their hideout. When you find them, defeat those monsters!
+					</p>
+					<p>
+						<strong>Travel -</strong> Sometimes all you need is a short trip. Take a jaunt across town or up the road awhile and see what there is to see.
 					</p>
 					</div>
 					
 				</div>
 				<div class="gamesInfo TravelQuestDesc" id="three">
-					<div style="width:400px">
-					<p>
-						&nbsp;&nbsp;&nbsp;&nbsp;Added this real slick like
-					</p>
+					<img src='./img/ISpyderColor.png' style='display:inline-block;height:100%;'/>
+					<div style="position:absolute;width:25%;left:27%;display:inline-block;">
+						<p>&nbsp;&nbsp;&nbsp;&nbsp;What is there to say? Nobody really knows where these ones came from. . . Probably the remnant of an ill-advised project 
+						from one of the mega-corporations. . . In any case, the eye-spiders were once friendly, helpful even. They would do math, send messages, and even 
+						play games. . . That was all before. . . A few years back they turned, and we were honestly lucky to fight them off into the wilderness. They're savage 
+						beasts now, dangerous and with only ill intent.</p>
+						<p>Worst part is. . . They all talk in this vaguely sarcastic voice. . . It's creepy.</p>
 					</div>
 					
 				</div>
@@ -77,13 +113,20 @@
 			<div class="games_div_opacity">
 
 			</div>
-			<div style="z-index:10;width:100%;min-width:765px;height:250px;background:url('./img/tqBanner.png') no-repeat center center;background-size:100% auto;">
+			<div style="z-index:10;width:100%;min-width:765px;height:250px;background:url('./img/LHBanner.png') no-repeat center center;background-size:100% auto;">
 				
 			</div>
+			
+
+			
 			<div class="buttons">
 				<div class="button LostHopeDesc " id="one"><i class="fa fa-bookmark fa-2x fa-fw" style=""></i></div>
 				<div class="button LostHopeDesc " id="two"><i class="fa fa-exclamation fa-2x fa-fw" style=""></i></div>
 				<div class="button LostHopeDesc " id="three"><i class="fa fa-paw fa-2x fa-fw" style=""></i></div>
+			</div>
+			<div class="gamesQuick" id="LostHopeTitle">
+				<strong>Release Date:</strong> In good time</br>
+				<strong>Platforms:</strong> PC
 			</div>
 			<div class="contents">
 				<div class="gamesInfo LostHopeDesc" id="one">
