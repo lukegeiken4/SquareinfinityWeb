578853a88f092bc6bb495c9bf4ee99d3ac4f035c
Ivan Mattie <ivan.mattie@squareinfinity.com>
2014-07-16 11:44:04 -0600

    Added Portfolio Templates

--- tech_new/portfolio.php
+++ tech_new/portfolio.php
@@ -6,12 +6,36 @@
 			getHead("Square Infinity Portfolio");
 		?>
 		<link href="portfolio.css" rel="stylesheet" type="text/css" />
+		
+		<script>
+			$(document).ready(function(){
+				
+			});
+		</script>
 	</head>
 	<body onload="init();">	
 		<?php getTopBar(); ?>
 		
 		<!-- CONTENT STARTS HERE -->
-	
 		
+		<div class="portfolio-container">
+			
+			<div class="portfolio-entry" name="portfolio-part" style="background: url('http://crispme.com/wp-content/uploads/old-city.jpg?pass');">
+				<div class="portfolio-content" name="portfolio-entry">
+					<i class="fa fa-globe fa-2x portfolio-icon"></i>
+					<h1>A Web Job</h1>
+					<h2>This is an example web job that we have done.</h2>
+				</div>
+			</div>
+			
+			<div class="portfolio-entry" name="portfolio-part" style="background: url('http://crispme.com/wp-content/uploads/254497-1920x1080.jpg?pass');">
+				<div class="portfolio-content" name="portfolio-entry">
+					<i class="fa fa-mobile fa-2x portfolio-icon"></i>
+					<h1>An App Job</h1>
+					<h2>This is an example of an app we have designed.</h2>
+				</div>
+			</div>
+			
+		</div>
 	</body>
 </html>
