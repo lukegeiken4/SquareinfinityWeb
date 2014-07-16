5d074cb7470ad97c0dde63cb0b3e34c5375cb214
Luke Geiken <lukedrum4@gmail.com>
2014-07-16 10:58:17 -0600

    Team can now transfer to mobile

--- games/team.php
+++ games/team.php
@@ -8,23 +8,24 @@
 <?php getMainBar('The Party'); ?>
 	
 	<div style="position: absolute; width:100%; height:100%; top:0; z-index: 0;">
-		<div class="team_profile"  style="z-index: -5;">
+		<div class="team_collage-div"  style="z-index: -5;">
 			<div style="position: absolute;  background: black; width:70%; height:80%; left: 0; right: 0; margin-top:10%; z-index: -3; margin-left: auto; margin-right: auto; opacity: .8;"></div>
 			<div style="position: absolute; width:100%; height:25%; background-color: white; margin-top:5%; z-index: -2;">
 				<h1 style="text-align: center; font-size: 100px; margin-top:3%;">Meet The Team</h1>
 			</div>
 			<div style="width:100%; height:100%; ">
-				<img src="img/team_collage.png" style="padding-top:12%; display: block; margin-left:auto; margin-right:auto;"/>
+				<img src="img/team_collage.png" id="team-collage" style="padding-top:12%; display: block; margin-left:auto; margin-right:auto;"/>
 			</div>
-		<div class="team_profile">
+		</div>
+		<div class="team_profile" id="first_profile">
 			<div class="team_content">
 				<img src="img/ivanMattie.jpg" class="profile_image"/>
 				<div class="team_name_div">
-					<h1 style="padding-top:1%; padding-left:15%; font-size: 100px;">Ivan <span style="font-size:40px;">Mattie</span></h1>
-					<span style="padding-left:15%;"><a href="https://twitter.com/aeolingamenfel" class="twitter-follow-button" style="left:10px;" data-related="Ivan" data-show-count="true">Follow Ivan</a></span>
+					<h1 id="fname">Ivan </h1><h1 id="lname">Mattie</h1>
+					<span id="twitter-follower"><a href="https://twitter.com/aeolingamenfel" class="twitter-follow-button" style="left:10px;" data-related="Ivan" data-show-count="true">Follow Ivan</a></span>
 				</div>
 				<div class="team_member_desc">
-					<h2 style="padding-top:5%; padding-bottom:2%; color:white;">
+					<h2 style="padding-top:5%; padding-bottom:5%; color:white;">
 						&nbsp;&nbsp;&nbsp&nbsp;Hello, I am a Computer Science major at the University of Colorado, at Colorado Springs. My major passions are telling stories and writing code, so naturally Square 
 						Infinity is an extension of of these passions. My goal is to design cool, interesting new software and applications, as well as design interesting games. I have a lot of ideas I would love
 						to share with the world, and with the team I'm with, I'm sure anything is possible. I hope to further games as a new, powerful medium for art and complex narrative. I also love the outdoors,
@@ -37,11 +38,11 @@
 			<div class="team_content">
 				<img src="img/davidLindenbaum.jpg" class="profile_image"/>
 				<div class="team_name_div">
-					<h1 style="padding-top:1%; padding-left:15%; font-size: 100px;">David <span style="font-size:40px;">Lindenbaum</span></h1>
-					<span style="padding-left:15%;"><a href="https://twitter.com/Da5id2701" class="twitter-follow-button" style="left:10px" data-related="David" data-show-count="true">Follow David</a></span>
+					<h1 id="fname">David <h1 id="lname">Lindenbaum</h1>
+					<span id="twitter-follower"><a href="https://twitter.com/Da5id2701" class="twitter-follow-button" style="left:10px" data-related="David" data-show-count="true">Follow David</a></span>
 				</div>
 				<div class="team_member_desc">
-					<h2 style="padding-top:5%; padding-bottom:2%; color:white;">
+					<h2 style="padding-top:5%; padding-bottom:5%; color:white;">
 						&nbsp;&nbsp;&nbsp&nbsp;I am a Computer Science major at Carnegie Mellon University. I have a long-standing passion for writing code, and above all I love a challenge. Square Infinity gives
 						me constant opportunities to challenge myself with interesting projects and produce real, useful products at the same time. I am excited to explore new technologies and create unique
 						applications and games with the rest of the team. I have particular experience with Android and Java development, as well as a very strong background in algorithms and theoretical computer
@@ -54,11 +55,11 @@
 			<div class="team_content">
 				<img src="img/andrewEagle.jpg"  class="profile_image"/>
 				<div class="team_name_div">
-					<h1 style="padding-top:1%; padding-left:15%; font-size: 100px;">Andrew <span style="font-size:40px;">Eagle</span></h1>
-					<span style="padding-left:15%;"><a href="https://twitter.com/ace_the_eagle" class="twitter-follow-button" style="left:10px" data-related="Andrew" data-show-count="true">Follow Andrew</a></span>				
+					<h1 id="fname">Andrew <h1 id="lname">Eagle</h1>
+					<span id="twitter-follower"><a href="https://twitter.com/ace_the_eagle" class="twitter-follow-button" style="left:10px" data-related="Andrew" data-show-count="true">Follow Andrew</a></span>				
 				</div>
 				<div class="team_member_desc">
-					<h2 style="padding-top:5%; padding-bottom:2%; color:white;">
+					<h2 style="padding-top:5%; padding-bottom:5%; color:white;">
 						&nbsp;&nbsp;&nbsp;&nbspI am currently studying Game Design as well as Computer Security in the University of Colorado at Colorado Springs' Bachelor of Innovation program. My goal with Square Infinity is to 
 						not only produce quality software for utility, but also to design and produce games that tell interesting, meaningful stories in new ways. I hope that I can change the way people think
 						about games for the better and help to hasten their acceptance into a wider range of arts. I met the rest of the team while attending Billings West High School and although we are now
@@ -71,11 +72,11 @@
 				<div class="team_content">
 				<img src="img/garretMeier.jpg"  class="profile_image"/>
 				<div class="team_name_div">
-					<h1 style="padding-top:1%; padding-left:15%; font-size: 100px;">Garret <span style="font-size:40px;">Meier</span></h1>
-					<span style="padding-left:15%;"><a href="https://twitter.com/headin_thecloud" class="twitter-follow-button" style="left:10px" data-related="Garret" data-show-count="true">Follow Garret</a></span>
+					<h1 id="fname">Garret <h1 id="lname">Meier</h1>
+					<span id="twitter-follower"><a href="https://twitter.com/headin_thecloud" class="twitter-follow-button" style="left:10px" data-related="Garret" data-show-count="true">Follow Garret</a></span>
 				</div>
 				<div class="team_member_desc">
-					<h2 style="padding-top:5%; padding-bottom:2%; color:white;">
+					<h2 style="padding-top:5%; padding-bottom:5%; color:white;">
 						&nbsp;&nbsp;&nbsp;&nbsp;Hi, I'm currently a freshman studying Software Engineering and Philosophy at Iowa State University. If you want to learn more about me personally have a look at 
 						<a href="http://ghmeier.public.iastate.edu" target="_blank">my website</a>. I can't wait to see what the team can do with Square Infinity. Right now, helping other
 						companies express themselves through technology really makes me excited to get to work on every unique project. I am currently working mostly on web design and Android
@@ -89,11 +90,11 @@
 				<div class="team_content">
 				<img src="img/luke_pic.png"  class="profile_image"/>
 				<div class="team_name_div">
-					<h1 style="padding-top:1%; padding-left:15%; font-size: 100px;">Lucas <span style="font-size:40px;">Geiken</span></h1>
+					<h1 id="fname">Lucas <h1 id="lname">Geiken</h1>
 					<!--<span style="padding-left:15%;"><a href="https://twitter.com/headin_thecloud" class="twitter-follow-button" style="left:10px" data-related="Garret" data-show-count="true">Follow Garret</a></span>-->
 				</div>
 				<div class="team_member_desc">
-					<h2 style="padding-top:5%; padding-bottom:2%; color:white;">
+					<h2 style="padding-top:5%; padding-bottom:5%; color:white;">
 						&nbsp;&nbsp;&nbsp;&nbsp;I love lamp.
 					</h2>
 				</div>
@@ -103,11 +104,11 @@
 				<div class="team_content">
 				<img src="img/taylor_pic.png"  class="profile_image"/>
 				<div class="team_name_div">
-					<h1 style="padding-top:1%; padding-left:15%; font-size: 100px;">Taylor <span style="font-size:40px;">Brown</span></h1>
+					<h1 id="fname">Taylor <h1 id="lname">Brown</h1>
 					<!--<span style="padding-left:15%;"><a href="https://twitter.com/headin_thecloud" class="twitter-follow-button" style="left:10px" data-related="Garret" data-show-count="true">Follow Garret</a></span>-->
 				</div>
 				<div class="team_member_desc">
-					<h2 style="padding-top:5%; padding-bottom:2%; color:white;">
+					<h2 style="padding-top:5%; padding-bottom:5%; color:white;">
 						&nbsp;&nbsp;&nbsp;&nbsp;Hey, I don't have a facebook.
 					</h2>
 				</div>
