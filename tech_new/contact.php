dfe6f4d183e3fdc0eda424750cff3357867cd3a8
Ivan Mattie <ivan.mattie@squareinfinity.com>
2014-07-16 17:47:05 -0600

    Finished WordPress Quoting

--- tech_new/contact.php
+++ tech_new/contact.php
@@ -104,24 +104,233 @@
 					<button class="large-button" id="submit-type-button" onclick="selectSiteType();">Continue</button>
 				</div>
 				
+				<div id="quote-web-area-final_notsure" hidden>
+					
+				</div>
+				
 				<div id="quote-web-area-final_wordpress" hidden>
 					<h1 id="quote-web-area-final-header">I Want a Website Made With Wordpress</h1>
 					<div class="information-small">
-						WordPress? Awesome! We just need a little more information from you
+						<a href="http://wordpress.org/">WordPress</a>? Awesome! We just need a little more information from you
 						to try and make a quote for you. If you don't specify an option, we
 						can always discuss or suggest one for you.
 					</div>
 					<h2>I want it with...</h2>
 					<div class="quote-selector-small">
 						<input type="checkbox" />This Theme:
-						<input type="text" placeholder="theme name" disabled />
+						<input type="text" placeholder="theme name" />
 					</div>
 					<div class="quote-selector-small">
 						<input type="checkbox" />These Plugins:
-						<input type="text" placeholder="list of some plugins" disabled />
+						<input type="text" placeholder="list of some plugins" />
+					</div>
+					<div class="quote-selector-small">
+						<input type="checkbox" id="wordpress_ecommerce-check"/> eCommerce
+					</div>
+					<hr width="40%"/>
+					<div class="quote-selector-small">
+						<input type="checkbox" id="wordpress_content-setup-check" /> I Want Square Infinity to Setup My Content
+						<div class="information-small tab">
+							Checking this means you don't want to put the beginning content
+							of your site into the site itself, and you want Square Infinity
+							to do it for you, keeping in mind you must provide said content.
+							This is free, unless you have a large amount of content that 
+							would take a lot of our time to do.
+						</div>
+					</div>
+					<div class="quote-selector-small">
+						<input type="checkbox" /> I Need Help With WordPress
+						<div class="information-small tab">
+							Checking this means you are not familar with
+							WordPress, or just want some more training
+							on how to use it, which we will provide free
+							of charge.
+						</div>
+					</div>
+					<hr width="40%"/>
+					<div class="quote-selector-small">
+						<input type="checkbox" id="wordpress_domain-needed" /> I Need a Domain or Domains
+						<div class="information-small tab">
+							Checking this means you do not own
+							the domain you want your site hosted
+							on, and you want Square Infinity to 
+							purchase the domain in your name. Keep
+							in mind some domains are already registered
+							and may be much more expensive or unavailable.
+						</div>
+					</div>
+					<div class="quote-selector-small">
+						<input type="checkbox" id="wordpress_si-host" /> I Want Square Infinity to Host My Site
+						<div class="information-small tab">
+							Checking this means you do not have a server
+							or hosting provider to host your site on, 
+							and want to host with Square Infinity. We
+							offer tiered hosting options to our client 
+							that start at $3/mo.
+						</div>
+						<div class="information-small tab">
+							You will not be billed for hosting until
+							completion of site design.
+						</div>
+					</div>
+					<hr width="40%"/>
+					<div class="quote-selector-small">
+						<input type="checkbox" checked disabled/> WordPress Essentials
+						<div class="information-small">
+							WordPress and Square Infinity provide some features free of charge with
+							every WordPress design we make, which includes:<br />
+							<div class="information-bullet"><i class="fa fa-check"></i>Search Engine Optimization</div>
+							<div class="tab">
+								what's a site if it doesn't show up
+								on a search engine correctly? That's why we make sure to use the
+								necessary meta tags, robots.txt, and social media linking to make sure
+								your users find you.
+							</div>
+							<div class="information-bullet"><i class="fa fa-check"></i>Social Media Integration</div>
+							<div class="tab">
+								In today's interconnected, social media centered digital world, it's important
+								to keep brand standards in tact, and have cross pollination between your social
+								networks and your website, and we will help you to add Twitter feeds, Facebook
+								like buttons, or anything you could need.
+							</div>
+						</div>
+					</div>
+					<h2>Some Final Information</h2>
+					<div class="information-small">
+						We just need a little more information from you to really get an accurate quote.
+						Please describe your site design in more detail below.
+					</div>
+					<div class="information-medium">
+						What is this site design for?
+					</div>
+					<div class="quote-selector-small">
+						<textarea class="final-information" placeholder="My site design is for my company XYZ Co. and..."></textarea>
+					</div>
+					<div class="information-medium">
+						Is there anything specific you need or want this site to do?
+					</div>
+					<div class="quote-selector-small">
+						<textarea class="final-information" placeholder="Well I do need it to have..."></textarea>
+					</div>
+					<h2>Estimated Pricing</h2>
+					<div class="information-small">
+						Below is an estimate of how much this design would cost you.
+						It is just an estimate based on how much we have charged in
+						the past for similar designs, and may not be reflective
+						of what we would actually charge. This price is, of course,
+						totally negotiable, and we can work out custom solutions
+						if necessary to accommodate your needs.
+					</div>
+					<div>
+						<div class="price-entry-name">
+							WordPress Setup
+						</div>
+						<div class="price-entry">
+							$25-100
+						</div>
+						<br />
+						<div class="price-entry-name">
+							Theme Construction/Modification
+						</div>
+						<div class="price-entry">
+							$200-800
+						</div>
+						<br />
+						<div id="wordpress_price-area">
+							<!-- this will be autofilled -->
+						</div>
+						<div class="price-entry-name">
+							WordPress Essentials
+						</div>
+						<div class="price-entry">
+							Free!
+						</div>
+						<hr width="50%" />
+						<div id="wordpress_price-area-final" class="final-price">
+							<!-- this will also be autofilled -->
+						</div>
 					</div>
 					
-					<button class="large-button" id="submit-type-button" onclick="">Get Quote</button>
+					<button class="large-button" id="submit-type-button" onclick="updateWordpressPricing();">Refresh Price</button>
+					
+					<h2>Delivery Time</h2>
+					<div class="information-medium">
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						For most WordPress sites, we can have your site fully
+						functional and on any domain you own in 1-2 weeks.
+						Naturally, the more content you have, or the more custom
+						you want the site to be, the longer it may take.
+					</div>
+					<div class="information-medium">
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						If you have a rush job or generally need the
+						site done quick, we can accommodate, but if
+						we have a lot of other sites we are designing
+						at the time, you may be charged extra.
+					</div>
+					<h2>Want a More Accurate Quote?</h2>
+					<div class="information-medium">
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						This system is entirely just to give you an
+						idea of generally what we charge. If you
+						want a more accurate quote, just click the
+						button below and we will personally look at your request
+						and give you a quote.
+					</div>
+					<button class="large-button" id="submit-type-button" onclick="showPersonalInformation('quote-web-area-final_wordpress');">
+						Get Quote
+					</button>
+				</div>
+				
+				<div id="quote-personal-information" hidden>
+					<h1>Your Information</h1>
+					<div class="information-medium">
+						Before we look over your quote request,
+						we need some information about you, so 
+						we can contact you when we get you a quote.
+					</div>
+					<div class="information-small">
+						Fields marked with a (*) are required.
+					</div>
+					<h2>Personal Information</h2>
+					<div class="information-small">
+						Below is the information of the person
+						we will contact when we finish the quote,
+						or if we have any questions about your
+						quote.
+					</div>
+					<div class="quote-selector-small">
+						Name*: <input type="text" />
+					</div>
+					<div class="quote-selector-small">
+						Phone: <input type="text" />
+					</div>
+					<div class="quote-selector-small">
+						Email*: <input type="text" />
+					</div>
+					<div class="quote-selector-small">
+						Contact Hours: <input type="text" />
+						<div class="information-small tab">
+							If you fill this in, we will only
+							contact you via telephone or email
+							within the time restriction listed.
+						</div>
+						<div class="information-small tab">
+							In most cases, we will never contact
+							you outside of normal business hours.
+						</div>
+					</div>
+					<h2>Association Information</h2>
+					<div class="quote-selector-small">
+						Company/Assoc Name: <input type="text" />
+					</div>
+					<div class="quote-selector-small">
+						Website/Domain: <input type="text" />
+					</div>
+					
+					<button class="large-button">
+						Get Quote
+					</button>
 				</div>
 			</div>
 			
--- tech_new/js/contact.js
+++ tech_new/js/contact.js
@@ -32,6 +32,61 @@ selectSiteType = function(){
 	}
 }
 
+showPersonalInformation = function(caller){
+	$("#" + caller).hide(animSpeed);
+	
+	$("#quote-personal-information").show(animSpeed);
+}
+
+updateWordpressPricing = function(){
+	var priceArea = document.getElementById("wordpress_price-area");
+	var finalPriceArea = document.getElementById("wordpress_price-area-final");
+	
+	var ecommerce = document.getElementById("wordpress_ecommerce-check");
+	var contentSetup = document.getElementById("wordpress_content-setup-check");
+	var domainNeeded = document.getElementById("wordpress_domain-needed");
+	var siHost = document.getElementById("wordpress_si-host");
+	
+	priceArea.innerHTML = "";
+	
+	var finalLowPrice = 225;
+	var finalHighPrice = 900;
+	
+	if(ecommerce.checked){
+		finalLowPrice += 400;
+		finalHighPrice += 1000;
+		
+		addPrice(priceArea, "eCommerce", "$400-1000");
+	}if(contentSetup.checked){
+		finalHighPrice += 500;
+		
+		addPrice(priceArea, "Content Setup", "$0-500");
+	}if(domainNeeded.checked){
+		finalLowPrice += 10;
+		finalHighPrice += 50;
+		
+		addPrice(priceArea, "Domain Registration", "$10-50");
+	}if(siHost.checked){
+		addPrice(priceArea, "Hosting (Alpha Tier)", "$3/mo");
+	}
+	
+	finalPriceArea.innerHTML = "Total: $" + finalLowPrice + "-" + finalHighPrice;
+}
+
+addPrice = function(div, name, price){
+	var priceNameDiv = document.createElement("div");
+	priceNameDiv.setAttribute("class", "price-entry-name");
+	priceNameDiv.innerHTML = name;
+	
+	var priceDiv = document.createElement("div");
+	priceDiv.setAttribute("class", "price-entry");
+	priceDiv.innerHTML = price;
+	
+	div.appendChild(priceNameDiv);
+	div.appendChild(priceDiv);
+	div.appendChild(document.createElement("br"));
+}
+
 show = function(name){
 	var wordpressArea = $("#quote-wordpress-area");
 	var laravelArea = $("#quote-laravel-area");
