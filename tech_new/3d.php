c8092671157f663f4fb8908aff9236f6ef4446f4
Ivan Mattie <ivan.mattie@squareinfinity.com>
2014-07-18 14:05:22 -0600

    Implemented Sticky Note Button

--- tech_new/3d.php
+++ tech_new/3d.php
@@ -141,10 +141,12 @@
 			}
 			?>
 		</div>
-		<div id="sticky-notes" style="position: absolute;background: url(img/papernotes.png); background-size: contain; margin-top:-700px;">
-			&nbsp;
-		</div>
-		<div id="sticky-notes" style="position: absolute;background: url(img/papernotes3.png); background-size: contain; margin-top:-700px; right:100px;">
+		<button id="sticky-note">
+			<div class="note-interior">
+				Our Portfolio
+			</div>
+		</button>
+		<div id="sticky-notes" style="position: absolute;background: url('img/papernotes3.png'); background-size: contain; margin-top:-700px; right:100px;">
 			&nbsp;
 		</div>
 		<div class="coffee side-spacing-1" id="coffee">
